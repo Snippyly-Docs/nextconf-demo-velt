40c441c1bdddaf7c7460ee9f2312972c2dc6761a
benjvelt <benjamin@velt.dev>
2023-10-26 13:02:18 -0700

    repo url

--- src/components/YourDocument.js
+++ src/components/YourDocument.js
@@ -1,4 +1,4 @@
-import { useVeltClient, VeltCommentTool, VeltPresence, VeltHuddleTool,VeltHuddle } from "@veltdev/react";
+import { useVeltClient, VeltCommentTool, VeltPresence } from "@veltdev/react";
 import { useEffect, useState } from "react";
 import YourAuthComponent from "./YourAuthComponent";
 
@@ -19,18 +19,18 @@ export default function YourDocument() {
   }, [client]);
 
   return (
-    <div class="text-white flex h-screen w-screen items-center justify-center" style={{background:`${color}`}}>
+    <div className="text-white flex h-screen w-screen items-center justify-center" style={{background:`${color}`}}>
         
-    <div class="relative flex w-[40rem] flex-col justify-center overflow-hidden rounded-lg bg-[#404040] bg-opacity-30 py-32">
+    <div className="relative flex w-[40rem] flex-col justify-center overflow-hidden rounded-lg bg-[#404040] bg-opacity-30 py-32">
         
         <div className="mb-[64px] flex justify-center">
             <VeltPresence flockMode={true}/>
         </div>
-       <div class="flex flex-col items-center">
+       <div className="flex flex-col items-center">
             <YourAuthComponent/>
-        <h1 class="text-[32px] font-medium ">Tap <span className="text-[#908bff]">comment</span> to get started</h1>
+        <h1 className="text-[32px] font-medium ">Tap <span className="text-[#908bff]">comment tool</span> to get started</h1>
   
-        <div class="text-center">
+        <div className="text-center">
             <div className="m-[32px]">
             <p className="font-bold m-4 text-[12px]">COMMENT MODE</p>
                 <div className="flex justify-center">
@@ -40,22 +40,7 @@ export default function YourDocument() {
                     </div>
                 </div>
             </div>
-          <div className="m-[32px]">
-          <p className="font-bold m-4 text-[12px]">HUDDLE MODE</p>
-            <div className="flex justify-center">
-                <div className="bg-opacity-40 bg-[#404040] w-[32px] h-[32px] rounded-full">
-                <VeltHuddleTool/>
-
-                </div>
-            </div>
-          </div>
-
-          <h1 class="text-[20px] mt-[20px] mb-[60px] font-light">Highlight this text to add a comment</h1>
-
-       
-
-          
-          
+          <h1 className="text-[20px] mt-[20px] mb-[60px] font-light">Highlight this text to add a comment</h1>
           
         </div>
   
